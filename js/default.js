e38593a5ff2ec659d13300975701b01f7ec8c936
liyanguestc <liyanguestc@gmail.com>
2015-01-27 15:14:57 +0800

    holoide commit

--- js/default.js
+++ js/default.js
@@ -72,6 +72,7 @@ function animate() {
         scene: scene,
         camera: camera,
         holoScreenSize: _holoScreenSize,
+        tunedsp:_maxDisparity,
         filterA: _filterA,
         filterB: _filterB,
         filterC: _filterC,
